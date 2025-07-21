7d6a52a99b33dfd4642dc6b0d26d7983c06e558a
munneth <munnethgill@gmail.com>
2025-07-20 18:50:17 -0700

    format and ran prettier

--- src/app/(data)/blogData.jsx
+++ src/app/(data)/blogData.jsx
@@ -1,8 +1,22 @@
-
 const posts = [
-    {id: 1, title: "Blog Post 1", content: "This is the content of blog post 1", slug: "blog-post-1"},
-    {id: 2, title: "Blog Post 2", content: "This is the content of blog post 2", slug: "blog-post-2"},
-    {id: 3, title: "Blog Post 3", content: "This is the content of blog post 3", slug: "blog-post-3"},
-]
+  {
+    id: 1,
+    title: 'Blog Post 1',
+    content: 'This is the content of blog post 1',
+    slug: 'blog-post-1',
+  },
+  {
+    id: 2,
+    title: 'Blog Post 2',
+    content: 'This is the content of blog post 2',
+    slug: 'blog-post-2',
+  },
+  {
+    id: 3,
+    title: 'Blog Post 3',
+    content: 'This is the content of blog post 3',
+    slug: 'blog-post-3',
+  },
+];
 
 export default posts;
